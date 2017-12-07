d4ac37ad817fe04a7220de365aacdf8bf1b04111
yegithub <yegithub@users.noreply.github.com>
2017-12-07 10:33:52 +0800

    Update Program.cs

--- AutoLevelUp/Program.cs
+++ AutoLevelUp/Program.cs
@@ -650,7 +650,7 @@ namespace AutoLevelUp
                     AbilitySequence = new[] { 1, 2, 3, 1, 1, 4, 1, 3, 1, 3, 4, 3, 3, 2, 2, 4, 2, 2 };
                     break;
                 case "Zoe":
-                    AbilitySequence = new[] { 1, 3, 2, 1, 1, 4, 1, 3, 1, 3, 4, 3, 3, 2, 2, 4, 2, 2 };
+                    AbilitySequence = new[] { 1, 3, 1, 2, 1, 4, 1, 3, 1, 3, 4, 3, 3, 2, 2, 4, 2, 2 };
                     break;
                 case "Zyra":
                     AbilitySequence = new[] { 3, 2, 1, 3, 1, 4, 3, 1, 3, 1, 4, 3, 1, 2, 2, 4, 2, 2 };
